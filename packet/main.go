ba6210f3110071f980c769529bc4f12879081850
weiyuexin <3022422894@qq.com>
2023-03-13 13:41:30 +0800

    1

--- packet/main.go
+++ packet/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"github.com/google/gopacket"
+	"github.com/google/gopacket/layers"
 	"github.com/google/gopacket/pcap"
 	"log"
 )
@@ -24,8 +25,60 @@ func main() {
 	// Loop through packets in file
 	packetSource := gopacket.NewPacketSource(handle, handle.LinkType())
 	for packet := range packetSource.Packets() {
-		//fmt.Println(packet.Data())
-		//fmt.Println("-----------------------------")
+		fmt.Println("-----------------------------")
+		fmt.Println(packet.Dump())
+		fmt.Println("-----------------------------")
 		fmt.Println(packet)
+		// 判断数据包是否为以太网数据包，可解析出源mac地址、目的mac地址、以太网类型（如ip类型）等
+		ethernetLayer := packet.Layer(layers.LayerTypeEthernet)
+		if ethernetLayer != nil {
+			fmt.Println("Ethernet layer detected.")
+			ethernetPacket, _ := ethernetLayer.(*layers.Ethernet)
+			fmt.Println("------------------------------------------")
+			fmt.Println(ethernetPacket)
+			fmt.Println("------------------------------------------------")
+			fmt.Println("Source MAC: ", ethernetPacket.SrcMAC)
+			fmt.Println("Destination MAC: ", ethernetPacket.DstMAC)
+			// Ethernet type is typically IPv4 but could be ARP or other
+			fmt.Println("Ethernet type: ", ethernetPacket.EthernetType)
+			fmt.Println()
+		}
+		//判断数据包是否为IP数据包，可解析出源ip、目的ip、协议号等
+		/*ipLayer := packet.Layer(layers.LayerTypeIPv4)
+		if ipLayer != nil {
+			fmt.Println("IPv4 layer detected.")
+			ip, _ := ipLayer.(*layers.IPv4)
+			// IP layer variables:
+			// Version (Either 4 or 6)
+			// IHL (IP Header Length in 32-bit words)
+			// TOS, Length, Id, Flags, FragOffset, TTL, Protocol (TCP?),
+			// Checksum, SrcIP, DstIP
+			fmt.Printf("From %s to %s\n", ip.SrcIP, ip.DstIP)
+			fmt.Println("Protocol: ", ip.Protocol)
+			fmt.Println()
+		}*/
+		// 判断数据包是否为TCP数据包，可解析源端口、目的端口、seq序列号、tcp标志位等
+		/*tcpLayer := packet.Layer(layers.LayerTypeTCP)
+		if tcpLayer != nil {
+			fmt.Println("TCP layer detected.")
+			tcp, _ := tcpLayer.(*layers.TCP)
+			// TCP layer variables:
+			// SrcPort, DstPort, Seq, Ack, DataOffset, Window, Checksum, Urgent
+			// Bool flags: FIN, SYN, RST, PSH, ACK, URG, ECE, CWR, NS
+			fmt.Printf("From port %d to %d\n", tcp.SrcPort, tcp.DstPort)
+			fmt.Println("Sequence number: ", tcp.Seq)
+			fmt.Println()
+		}*/
+
+		// Iterate over all layers, printing out each layer type
+		/*fmt.Println("All packet layers:")
+		for _, layer := range packet.Layers() {
+			fmt.Println("- ", layer.LayerType())
+		}*/
+		// 判断layer是否存在错误
+		/*if err := packet.ErrorLayer(); err != nil {
+			fmt.Println("Error decoding some part of the packet:", err)
+		}*/
+
 	}
 }
